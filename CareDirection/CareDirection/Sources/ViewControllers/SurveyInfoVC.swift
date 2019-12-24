6b7475af23d0e5c1acd3b7bd9c011f642677e981
이주혁 <>
2019-12-24 21:20:27 +0900

    [CREATE] SurveyInfo Storyboard and VC

---
+++ CareDirection/CareDirection/Sources/ViewControllers/SurveyInfoVC.swift
@@ -0,0 +1,20 @@
+//
+//  SurveyInfoVC.swift
+//  CareDirection
+//
+//  Created by 이주혁 on 2019/12/24.
+//  Copyright © 2019 SwiftHelloWorld. All rights reserved.
+//
+
+import UIKit
+
+class SurveyInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}

c42f4c822c13992483a9b7701415b74cab258fc3
이주혁 <>
2019-12-25 21:55:12 +0900

    [UPDATE] complete survey

---
+++ CareDirection/CareDirection/Sources/ViewControllers/SurveyLifeStyleLastPointVC.swift
@@ -0,0 +1,23 @@
+//
+//  SurveyLifeStyleLastPointVC.swift
+//  CareDirection
+//
+//  Created by 이주혁 on 2019/12/25.
+//  Copyright © 2019 SwiftHelloWorld. All rights reserved.
+//
+
+import UIKit
+
+class SurveyLifeStyleLastPointVC: UIViewController {
+
+    @IBOutlet var nextBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nextBtn.makeRounded(cornerRadius: 25)
+    }
+    
+
+
+
+}

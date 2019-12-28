f031fee94963eb452886c072b7719d4690d845a2
2017085687 <jaeeunno1@naver.com>
2019-12-28 20:05:43 +0900

    [CREATE] ingredient study view

--- CareDirection/CareDirection/Sources/ViewControllers/TakingProductRegistVC.swift
+++ CareDirection/CareDirection/Sources/ViewControllers/TakingProductRegistVC.swift
@@ -18,6 +18,8 @@ class TakingProductRegistVC: UIViewController {
     
     @IBOutlet weak var datePickerView: UIDatePicker!
     
+    @IBOutlet weak var datePickerTotalView: UIView!
+    
     var takingProductList: [TakingProduct] = []
     
     override func viewDidLoad() {
@@ -25,31 +27,36 @@ class TakingProductRegistVC: UIViewController {
         
         // date pick button customize
         datePickButton.makeRounded(cornerRadius: 8)
-<<<<<<< HEAD
+        
         datePickButton.dropShadow(color: UIColor.darkGray, offSet: CGSize(width: 0, height: 1), opacity: 0.3, radius: 4)
-        //datePickButton.dropShadow(color: UIColor.darkGray, offSet: CGSize(width: 0, height: 1), opacity: 0.3, radius: 4)
         
         setTakingProduct()
         
         datePickerView.addTarget(self, action: #selector(changed), for: .valueChanged)
-=======
-        //datePickButton.dropShadow(color: UIColor.darkGray, offSet: CGSize(width: <#T##CGFloat#>, height: <#T##CGFloat#>), opacity: <#T##Float#>, radius: <#T##CGFloat#>)
->>>>>>> c1928d683ee34ab9faaaa027cd2413a0b79fba65
+        
+        datePickerTotalView.isHidden = true
     }
     
     @objc func changed() {
         let dateformatter = DateFormatter()
-        
+
         dateformatter.locale = NSLocale(localeIdentifier: "ko_KR") as Locale
         dateformatter.dateStyle = .long
         dateformatter.timeStyle = .none
         let date = dateformatter.string(from: datePickerView.date)
-        
+
         dateLabel.text = date
     }
-    
-    //back button action
+
+    // back button action
     @IBAction func backButtonClick(_ sender: Any) {
+    }
+    
+    // date pick button click action
+    @IBAction func datePickButtonClick(_ sender: Any) {
+        
+        datePickerTotalView.isHidden = false
+        datePickerView.isHidden = false
     }
     
     
